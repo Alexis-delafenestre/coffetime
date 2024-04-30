aa3c0aba18ee6eaa37133a4b9b2974db67618155
Alexis Delafenestre <alexis.delafenestre16@gmail.com>
2024-04-30 15:19:21 +0200

    CoffeTime

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Entity\Commentaire;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Security\CustomAuthenticationSuccessHandler;
+use ContainerFOAKfMN\getAjoutProduitService;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\Mapping as ORM; 
+
+
+class AccueilController extends AbstractController
+{
+    #[Route('/accueil', name: 'homepage')]
+    public function index(): Response
+    {
+        
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+
+    //               route vers liste produits 
+    #[Route('/liste-produits', name: 'liste_produits')]
+    public function listeProduits(EntityManagerInterface $manager): Response
+    {
+        $produitRepository = $manager->getRepository(Produit::class);
+        $tousProduits = $produitRepository->findAll();
+    
+        return $this->render('accueil/listeProduits.html.twig', [
+            'tousProduits' => $tousProduits,
+        ]);
+    }
+
+
+
+    #[Route('/produit/{id}', name: 'details_produit')]
+    public function detailsProduit(EntityManagerInterface $entityManager, $id): Response
+    {
+        $produitRepository = $entityManager->getRepository(Produit::class);
+        $produit = $produitRepository->find($id);
+
+        $commentaire = $produit->getCommentaire();
+
+        return $this->render('accueil/infoProduit.html.twig', [
+            'produit' => $produit,
+            'commentaire' => $commentaire,
+        ]);
+    }
+
+
+    #[Route('/produit/{id}/ajout-commentaire', name: 'ajout_commentaire', methods: ['POST'])]
+    public function addComment(Request $request, EntityManagerInterface $entityManager, $id): Response
+    {
+        $produit = $entityManager->getRepository(Produit::class)->find($id);
+
+        $titre = $request->request->get('titre');
+        $contenu = $request->request->get('contenu');
+
+        if (empty($titre) || empty($contenu)) {
+            $this->addFlash('error', 'Veuillez remplir tous les champs s\'il vous plaît.');
+    
+            return $this->redirectToRoute('details_produit', ['id' => $id]);
+        } else {
+        $commentaire = new Commentaire();
+        $commentaire->setTitre($titre);
+        $commentaire->setContenue($contenu);
+        $commentaire->setProduit($produit);
+
+        $entityManager->persist($commentaire);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Commentaire ajouté avec succès.');
+
+        return $this->redirectToRoute('details_produit', ['id' => $id]);
+        }
+    }
+
+
+    
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\DataFixtures;
+use app\entity\Produit;
+use app\entity\Commentaire; 
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+
+        //produit 1
+        $produit1 = new Produit();
+        $produit1->setNom('café moulue');
+        $produit1->setDescription('Café Parisien de très bonne qualité');
+        $produit1->setImage('images/coffee1.jpg');
+        $produit1->setStock('40');
+        $manager -> persist($produit1); 
+
+        //commentaire 1 lier produit 1
+        
+        $Commentaire1 = new Commentaire();
+        $Commentaire1 -> setTitre('très bon café');
+        $Commentaire1 -> setContenue('Lorem ipsum dolor sit amet, consectetur niristique senectus et netus et malesus nec pharetra lectus.');
+        $Commentaire1 -> setProduit($produit1);
+        $manager -> persist($Commentaire1); 
+
+
+        //produit 2
+        $produit2 = new Produit();
+        $produit2->setNom('cafetière');
+        $produit2->setDescription('Cafetière à piston');
+        $produit2->setImage('images/cafetiere-piston');
+        $produit2->setStock('200');
+        $manager -> persist($produit2); 
+
+        //commentaire 2 lier produit 2
+
+        $Commentaire2 = new Commentaire();
+        $Commentaire2 -> setTitre('Simple d\utilisation !');
+        $Commentaire2 -> setContenue('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, turpis vel tempus dignissim, nisl mauris lacinia eros, ut mattis odio lorem eget magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec pharetra lectus.');
+        $Commentaire2 -> setProduit($produit2);
+        $manager -> persist($Commentaire2); 
+
+        //Commentaire 3 lier produit 2
+
+        $Commentaire3 = new Commentaire();
+        $Commentaire3 -> setTitre('très belle cafetière');
+        $Commentaire3 -> setContenue('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, turpis vel tempus dignissim, nisl mauris lacinia eros, ut mattis odio lorem eget magna. Pellentesqu e habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec pharetra lectus.');
+        $Commentaire3 -> setProduit($produit2);
+        $manager -> persist($Commentaire3); 
+
+
+        //commentaire 4 lier produit 2
+        $Commentaire4= new Commentaire();
+        $Commentaire4-> setTitre('j\adore !');
+        $Commentaire4-> setContenue('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, turpis vel tempus dignissim, nisl mauris lacinia eros, ut mattis odio lorem eget magna. Pellentesqu e habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec pharetra lectus.');
+        $Commentaire4-> setProduit($produit2);
+        $manager -> persist($Commentaire4); 
+
+
+        //produit 3
+        $produit3 = new Produit();
+        $produit3->setNom('cuillère dosette à café');
+        $produit3->setDescription('Cuillère pour doser vos meilleurs cafés ');
+        $produit3->setImage('images/cuillere-mesure.webp');
+        $produit3->setStock('200');
+        $manager -> persist($produit3); 
+
+        //commentaire 5 lier produit 3
+
+        $Commentaire5 = new Commentaire();
+        $Commentaire5 -> setTitre('très utile pour doser le café !');
+        $Commentaire5 -> setContenue('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, turpis vel tempus dignissim, nisl mauris lacinia eros, ut mattis odio lorem eget magna. Pellentesqu e habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec pharetra lectus.');
+        $Commentaire5 -> setProduit($produit3);
+        $manager -> persist($Commentaire5); 
+
+        // commentaire 6 lier produit 3
+
+        $Commentaire6 = new Commentaire();
+        $Commentaire6 -> setTitre('très utile pour doser le café !');
+        $Commentaire6 -> setContenue('Lorem ipsum dolor sit ed vestibulum, turpis vel tempus dignissim, nisl mauris  abitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec pharetra lectus.');
+        $Commentaire6 -> setProduit($produit3);
+        $manager -> persist($Commentaire6); 
+
+
+
+
+
+        $produit3 = new Produit();
+        $produit3->setNom('cuillère à café');
+        $produit3->setDescription('Cette cuillère vous servira à doser votre cofé pour le préparer avec soin');
+        $produit3->setImage('public/images/cuillere-mesure.webp');
+        $produit3->setStock('350');
+        $manager -> persist($produit1); 
+        $Commentaire3 = new Commentaire();
+        $Commentaire3 -> setTitre('Simple d\utilisation !');
+        $Commentaire3 -> setContenue('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, turpis vel tempus dignissim, nisl mauris lacinia eros, ut mattis odio lorem eget magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec pharetra lectus.');
+        $Commentaire1 -> setProduit($produit2);
+        $manager -> persist($Commentaire2); 
+
+        $manager -> flush();
+        
+
+
+        
+
+
+}
+
+}
